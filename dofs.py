fdeb785d469e2fb417f9fa7e937184d03e77a6d1
fbresme-imperial <75369411+fbresme-imperial@users.noreply.github.com>
2023-10-30 21:28:36 +0000

    Added description

--- dofs.py
+++ dofs.py
@@ -1,25 +1,28 @@
 
 #%% 
 # The Bresme Group 2023
-# Calculate degrees of freedom for a given rigid molecule
+# Calculate degrees of freedom of each in a water molecule modelled with rigid bonds. 
+# The degrees of freedom can be used to calculate the atomic temperatures.
 # Reference: Olarte-Plata and Bresme, J. Chem. Phys. 2022, 156, 204701  doi: 10.1063/5.0090983
 #
 from math import degrees
 import numpy as np
 
-
 # SPC/E
 # r0 = 1.0 # Angstrom
 # theta0 = 109.47 # degrees
+
 # TIP3P, TIP4P, TIP4P/2005
 r0 = 0.9572 # Angstrom
 theta0 = 104.52 # degrees
 
 theta0_rad = theta0 * np.pi / 180. # radians
+
+# Atomic weight
 mh = 1.0078 # g/mol
 mo = 15.999 # g/mol
 
-
+# H coordinates
 xh =  r0*np.sin(theta0_rad/2)
 yh = -r0*np.cos(theta0_rad/2)
 print ("xh=", xh, " yh=", yh)
@@ -28,7 +31,6 @@ print ("xh=", xh, " yh=", yh)
 xcom = 0
 ycom = 2*mh*yh/(mo + 2*mh)
 print ("xcom=", xcom, " ycom=", ycom)
-
 
 # Moment of inertia
 Ix = 2*mh*xh**2
@@ -56,13 +58,15 @@ print ("vy=", vy)
 print ("vz=", vz)
 
 # Kinetic energy hydrogen
-
 Ek_h = mh/2*(vx**2 + vy**2 + vz**2) + mh/2*((wx*xh)**2 + (wy*(yh-ycom))**2 + (wz*np.sqrt(xh**2 + (yh-ycom)**2))**2)
 
 print ("Ek_h=", Ek_h) 
+
+# Kinetic energy oxygen
 Ek_o = 1/2*mo*(vx**2 + vy**2 + vz**2) + 1/2*mo*((wy*ycom)**2 + (wz*ycom)**2)
 print ("Ek_o=", Ek_o)
 
+# Number of degrees of freedom, oxygen and hydrogen
 Ndof_o = 2*Ek_o
 Ndof_h = 2*Ek_h
 
